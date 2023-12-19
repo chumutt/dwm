5cb3b1b6a8aa2403f3d80100e49833a7d9f494ee
ChuMutt <chufilthymutt@gmail.com>
2023-12-19 16:34:01 -0600

    fix

--- config.h
+++ config.h
@@ -50,7 +50,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	*/
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
-	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
+	{"Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
     {"Nextcloud", NULL,       NULL,             1 << 8,       0,           0,         0,        -1 },
     {NULL, NULL, "KeePassXC",                   1 << 8,       0,           0,         0,        -1 },
     {NULL, NULL, "RaySession",                  1 << 7,       0,           0,         0,        -1 },
